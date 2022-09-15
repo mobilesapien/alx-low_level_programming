3e0b512fbcd840d116967fb21df17d323089f52f
Hakeem Olatunji <hkmolatunji@gmail.com>
2022-09-14 18:23:27 -0700

    added

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - checks if the character is lower.
+ * return: returns 1 and 0 depending on condition
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+			return(0);
+}
