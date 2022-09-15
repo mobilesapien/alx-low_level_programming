87ed027a88a869235d149577856aa76b9bba3fde
Hakeem Olatunji <hkmolatunji@gmail.com>
2022-09-14 20:41:33 -0700

    ..

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  add - function that adds two integers and returns the resut
+ *  @a: init to be used to b
+ *  @b: init to be added to a
+ *  Retun: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
