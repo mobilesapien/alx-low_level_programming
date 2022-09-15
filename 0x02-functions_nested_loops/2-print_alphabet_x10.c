93b4b446c93ba674ac72b46aa48ed47bdf3a7f17
Hakeem Olatunji <hkmolatunji@gmail.com>
2022-09-14 18:03:01 -0700

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase, followed by a new line
+ *
+ * Return: 0
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char b;
+	int m;
+
+	m = 0;
+
+	while (m < 10)
+	{
+		b = 'a';
+		while (b <= 'z');
+		{
+			_putchar(b);
+			b++;
+		}
+		_putchar('\n');
+		m++;
+	}
+}
