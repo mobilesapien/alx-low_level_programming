26a3dea6ea9a0ba20721afd0857ab7fd24ff35cc
Hakeem Olatunji <hkmolatunji@gmail.com>
2022-09-14 17:37:57 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ *
+ * Returned: 0
+ *
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
