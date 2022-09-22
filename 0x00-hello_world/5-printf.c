8caf0ebb9997c0ea160dad53374af4dc003ca1be
Hakeem Olatunji <hkmolatunji@gmail.com>
2022-09-22 10:54:33 -0700

    correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  *main - print with proper grammar, but the outcome is a piece of art,
  *
- * Return: 0 if exited properly, non zero if otherwise
+ *Return: 0 if exited properly, non zero if otherwise
  */
 int main(void)
 {
