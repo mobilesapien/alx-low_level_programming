83624939451de3c6fa657598a5f211395213fa03
Hakeem Olatunji <35668520+mobilesapien@users.noreply.github.com>
2022-09-22 18:39:50 +0100

    correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- *main - print with proper grammar, but the outcome is a piece of art,
- *
- * Return: 0 if exited properly, non zero if otherwise
+ *main - Entry point
+ *My hello, printf task
+ *Return: return 1 after program execution
  */
 int main(void)
 {
-
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
